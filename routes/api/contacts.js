3847eee0d4f98170eb2403188a3820f18429a532
annared <annared1982@gmail.com>
2023-05-10 15:14:43 +0300

    updated PUT

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -38,6 +38,7 @@ router.post("/", async (req, res, next) => {
   try {
     const { error } = addSchema.validate(req.body);
     if (error) {
+      // throw HttpError(400, "missing required name field");
       throw HttpError(400, error.message);
     }
     const result = await contacts.addContact(req.body);
@@ -54,7 +55,7 @@ router.delete("/:contactId", async (req, res, next) => {
     if (!result) {
       throw HttpError(404, "Not found");
     }
-    res.json (result)
+    res.json({ message: "contact deleted" });
   } catch (error) {
     next(error);
   }
@@ -64,7 +65,7 @@ router.put("/:contactId", async (req, res, next) => {
   try {
     const { error } = addSchema.validate(req.body);
     if (error) {
-      throw HttpError(400, error.message);
+      throw HttpError(400, "missing fields");
     }
     const { contactId } = req.params;
     const result = await contacts.updateContact(contactId, req.body);
