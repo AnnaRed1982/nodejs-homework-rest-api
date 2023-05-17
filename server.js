bae74aacbaead5761af89afc6e8756036cd6a515
annared <annared1982@gmail.com>
2023-05-17 10:47:32 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
 const app = require("./app");
 
-const DB_HOST =
-  "mongodb+srv://Anna:Annared1982@cluster0.arsaudk.mongodb.net/db-contacts?retryWrites=true&w=majority";
+const { DB_HOST } = process.env;
+
 mongoose
   .connect(DB_HOST)
   .then(() => {
