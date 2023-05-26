8f124486aa058b60f035b2598b9f7ad2514e767b
annared <annared1982@gmail.com>
2023-05-26 17:38:29 +0300

    added filter favorite

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -4,13 +4,20 @@ const { HttpError, ctrlWrapper } = require("../helpers");
 
 const getAll = async (req, res) => {
   const { _id: owner } = req.user;
-  const { page = 1, limit = 20 } = req.query;
+  const { page = 1, limit = 20, favorite } = req.query;
   const skip = (page - 1) * limit;
   const result = await Contact.find({ owner }, "-createdAt -updatedAt", {
     skip,
     limit,
   }).populate("owner", "subscription email");
-  res.json(result);
+
+  if (favorite) {
+    res.json(
+      result.filter((contact) => contact.favorite.toString() === favorite)
+    );
+  } else {
+    res.json(result);
+  }
 };
 
 const getById = async (req, res) => {
